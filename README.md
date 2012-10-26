420b230bc8c9bd471cb117b7197d7a5706160e94
Parker Pinette <parker@parkerpinette.com>
2012-10-26 11:09:59 -0700

    fixed setup.py error and added to readme

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Pyramid Foundation
 
 This package provides a Pyramid scaffold which includes [Zurb Foundation](http://foundation.zurb.com/)
 
+pyramid\_foundation is intended to extend other scaffolds such as starter or alchemy. Using it alone will not create a Pyramid project.
+
+
+Getting Started
+---------------
+
+    $ pcreate -s <scaffold> -s pyramid_foundation <project_name>
+
+Your newly created project will have the Foundation files located in Project/package/static/ 
+
+Additionally, mytemplate.pt has been modified to include the Foundation CSS and JS along with the standard basic introduction to Foundation. 
+
+Currently, pyramid\_foundation provides the full foundation.css as well as the per-component files. Future support for SASS/Compass is planned. 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
@@ -15,14 +15,12 @@ setup(
     version='0.1',
     author='Parker Pinette',
     author_email='parker@parkerpinette.com',
-    packages=['pyramid_foundation'],
-    scripts=[''],
     url='https://github.com/ppinette/pyramid_foundation',
     license='LICENSE.txt',
     description='A Pyramid scaffold including Zurb Foundation',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
-        'Pyramid',
+        'pyramid',
     ],
     entry_points="""\
         [pyramid.scaffold]

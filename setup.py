c7f670a4695fbbd1648dbace8f9718b113bb8f20
Parker Pinette <parker@parkerpinette.com>
2012-12-13 10:45:41 -0800

    iterated to 0.2.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='pyramid_foundation',
-    version='0.2.4',
+    version='0.2.5',
     author='Parker Pinette',
     author_email='parker@parkerpinette.com',
     url='https://github.com/ppinette/pyramid_foundation',
